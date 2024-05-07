1f627941f225b90bb6b52d4ec40a6ec25a637f39
Toan Pham <toan.chpham@gmail.com>
2024-05-06 19:22:37 -0500

    add logo

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,8 +4,14 @@ import EmailPanel from '../EmailPanel/EmailPanel';
 
 function App() {
   return (
-    <div className="app user-select-none d-flex justify-content-center align-items-center flex-column body-text p-4" style={{ minHeight: '100vh' }}>
-      <EmailPanel />
+    <div className="app user-select-none d-flex flex-column justify-content-center align-items-center body-text p-4" style={{ minHeight: '100vh', position: 'relative' }}>
+      {/* Email Panel exactly centered */}
+      <div className="position-relative" style={{ width: '100%' }}>
+        <EmailPanel />
+      </div>
+
+      {/* Logo centered horizontally and dynamically positioned */}
+      <img className="logo position-absolute" src="https://get-taskie.app/logo.png" alt="Taskie Logo" style={{ left: '50%', transform: 'translate(-50%, -50%)' }} />
     </div>
   );
 }
